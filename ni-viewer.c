d9ced329e10e0e6c05c73040dc8dbdb44786f85e
rynsf <rynsf@users.noreply.github.com>
2024-11-05 23:50:53 +0530

    Add iff_find

--- ni-viewer.c
+++ ni-viewer.c
@@ -3,7 +3,9 @@
 #include "raylib.h"
 
 #define MAKE_ID( s )        ( (unsigned int)( ( s[0] << 24 ) | ( s[1] << 16 ) | ( s[2] << 8 ) | ( s[3] ) ) )
-#define IFF_FORM                MAKE_ID ( "FORM" )
+#define IFF_FORM            MAKE_ID ( "FORM" )
+#define IFF_FRAM            MAKE_ID("FRAM")
+#define IFF_NIMG            MAKE_ID("NIMG")
 
 typedef struct iff iff_t;
 struct iff {
@@ -14,7 +16,7 @@ struct iff {
     unsigned int reserved;
 };
 
-unsigned int f_get_w ( FILE *fh ) {
+unsigned int f_get_w(FILE *fh) {
     unsigned int ret;
 
     if ( !fh )
@@ -28,7 +30,7 @@ unsigned int f_get_w ( FILE *fh ) {
     return ret;
 }
 
-struct iff *iff_open ( FILE *fh ) {
+struct iff *iff_open(FILE *fh) {
     struct iff *iffh = NULL, *n = NULL, *p = NULL;
     unsigned int x = 0;
     unsigned int tlen = 0;
@@ -76,12 +78,25 @@ struct iff *iff_open ( FILE *fh ) {
     return iffh;
 }
 
+struct iff *iff_find(struct iff *prev, unsigned int type) {
+    if ( prev ) {
+        prev = prev->next;
+    }
+
+    while ( prev && ( prev->type != type ) ) {
+        prev = prev->next;
+    }
+
+    return prev;
+}
+
 int main(void) {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
     FILE *iffFile = fopen("8014.ni", "rb");
-    struct iff *i = iff_open(iffFile);
+    struct iff *iffh = iff_open(iffFile);
+    struct iff *img = iff_find(iffh, IFF_FRAM);
 
     InitWindow(screenWidth, screenHeight, "NI viewer");
     SetTargetFPS(60);
